d8afa57cd423c4fabfe2dc3b738b02f377983583
deltagare <deltagare@192.168.10.148>
2018-12-12 09:59:32 +0100

    Uploaded calculator to GitHub

---
+++ src/main/java/lexicon/se/maria/calculator/App.java
@@ -0,0 +1,13 @@
+package lexicon.se.maria.calculator;
+
+/**
+ * Calculator
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Calculation" );
+    }
+}
